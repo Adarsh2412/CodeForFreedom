8df28db827920c2fd684df71188aac0e651f69f5
Adarsh2412 <48621642+Adarsh2412@users.noreply.github.com>
2020-04-19 23:05:48 +0530

    Add files via upload

---
+++ Day1.py
@@ -0,0 +1,32 @@
+# Take input of size of list and the correct order of strings 
+num=int(input("Enter size of list: "))
+print("Enter the required list order")
+list_number=list(range(0,num))
+for i in range(0,num):
+    list_number[i]=int(input("Enter order: "))
+
+#Take input of strings
+string_list=list(range(0,num))
+print("Enter the Strings that need to be ordered")
+for i in range(0,num):
+    string_list[i]=input("Enter string number "+str(i+1)+" : ")
+
+#Bring String to correct order
+string2_list=list(range(0,num))
+for i in range(0,num):
+    string2_list[list_number[i]-1]=string_list[i]
+
+# Make necassary changes to each string as directed by the requirements
+for i in range(0,num):
+    if((i+1)==len(string2_list[i])):
+        if(string2_list[i]==string_list[i]):
+            continue
+        else:
+            string2_list[i]=string2_list[i].upper()
+    else:
+        string2_list[i]=string2_list[i].lower()
+
+#Print the final String in ordered format
+print("String in ordered format is: ")
+for i in string2_list:
+    print(i)
